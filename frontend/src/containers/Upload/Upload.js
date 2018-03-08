a5a691108390822ce8a6384568e5e0d24ec6b319
Mark Beasley <mbeasl1@gmail.com>
2018-03-07 23:39:37 -0500

    Import fix

--- frontend/src/containers/Upload/Upload.js
+++ frontend/src/containers/Upload/Upload.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-import { selectCollection } from 'redux/modules/user';
+import { selectCollection } from 'store/modules/user';
 
 import { UploadUI } from 'components/siteComponents';
 
